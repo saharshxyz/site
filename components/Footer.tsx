0afa22bf5246e8874ed309b58985439e4c7a5fdd
Saharsh Yeruva <hello@saharsh.tech>
2020-08-22 17:49:21 -0400

    Use react-icons instead of fontawsome

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,5 @@
 import Link from 'next/link';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faGithub,
-  faLinkedin,
-  faInstagram,
-} from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { FaGithub, FaInstagram, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 import footer from './Footer.module.scss';
 
 const Footer = () => {
@@ -22,29 +16,17 @@ const Footer = () => {
         </a>{' '}
         |{' '}
         <a href="https://srsh.link/github">
-          <FontAwesomeIcon
-            icon={faGithub}
-            className={`${footer.icon} ${footer.github}`}
-          />
+          <FaGithub className={`${footer.icon} ${footer.github}`} />
         </a>{' '}
         <a href="https://srsh.link/instagram">
-          <FontAwesomeIcon
-            icon={faInstagram}
-            className={`${footer.icon} ${footer.instagram}`}
-          />
+          <FaInstagram className={`${footer.icon} ${footer.instagram}`} />
         </a>{' '}
         <a href="https://srsh.link/linkedin">
-          <FontAwesomeIcon
-            icon={faLinkedin}
-            className={`${footer.icon} ${footer.linkedin}`}
-          />
+          <FaLinkedin className={`${footer.icon} ${footer.linkedin}`} />
         </a>{' '}
         <a href="mailto:hello@saharsh.tech">
           {' '}
-          <FontAwesomeIcon
-            icon={faEnvelope}
-            className={`${footer.icon} ${footer.email}`}
-          />{' '}
+          <FaEnvelope className={`${footer.icon} ${footer.email}`} />{' '}
         </a>
       </p>
     </div>
