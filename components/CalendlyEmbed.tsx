1ba7dbb8c303d3223d3d50a5ac14e4bcd4bccbaf
Saharsh Yeruva <hello@saharsh.tech>
2020-08-24 19:20:25 -0400

    Fix Calendly component not loading

--- components/CalendlyEmbed.tsx
+++ components/CalendlyEmbed.tsx
@@ -1,23 +1,27 @@
-import Head from 'next/head';
-
+import { useEffect } from 'react'
 const SCRIPT_SRC = 'https://assets.calendly.com/assets/external/widget.js';
-
 export interface CalendlyEmbedProps {
   account: string;
   eventName: string;
 }
-
-const CalendlyEmbed = ({ account, eventName }: CalendlyEmbedProps) => (
-  <div id="schedule_form">
-    <Head>
-      <script key="calendly-script" src={SCRIPT_SRC} async />
-    </Head>
-    <div
-      className="calendly-inline-widget"
-      data-url={`https://calendly.com/${account}/${eventName}`}
-      style={{ minWidth: '480px', height: '700px' }}
-    />
-  </div>
-);
-
+const createScript = () => {
+  const script = document.createElement('script');
+  script.src = SCRIPT_SRC;
+  return script;
+};
+const CalendlyEmbed = ({ account, eventName }: CalendlyEmbedProps) => {
+  useEffect(() => {
+    const script = document.body.appendChild(createScript())
+    return script.remove.bind(script)
+  }, [])
+  return (
+    <div id="schedule_form">
+      <div
+        className="calendly-inline-widget"
+        data-url={`https://calendly.com/${account}/${eventName}`}
+        style={{ minWidth: '480px', height: '700px' }}
+      />
+    </div>
+  );
+};
 export default CalendlyEmbed;
