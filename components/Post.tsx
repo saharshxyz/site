f96d8176fd4a7f6ab4088da844e1f7ded2c57f98
Saharsh Yeruva <hello@saharsh.tech>
2020-08-25 13:39:31 -0400

    Add propTypes to Post component

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,8 +1,14 @@
 import Link from 'next/link';
+import PropTypes from 'prop-types';
 import post from './Post.module.scss';
 
 const Post = (props) => {
   return <div className={post.root}>{props.children}</div>;
+};
+
+
+Post.propTypes = {
+  children: PropTypes.element.isRequired,
 };
 
 export default Post;
