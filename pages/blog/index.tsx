24de051d14cd74f78616a592ec35ef0a2757db10
Saharsh Yeruva <hello@saharsh.tech>
2020-08-29 23:17:59 -0400

    Added tag list

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,10 +1,12 @@
 import Layout from '../../components/Layouts/Layout';
 import Header from '../../components/Blog/Header';
+import Tags from '../../components/Blog/tags'
 
 const Home = () => {
   return (
     <Layout>
       <Header />
+      <Tags />
     </Layout>
   );
 };
