5ab1c98032519c127dedad36fef9a22502ce3777
Saharsh Yeruva <hello@saharsh.tech>
2020-08-29 20:50:46 -0400

    Add hovoer effects to form submit button

--- components/Blog/Form.tsx
+++ components/Blog/Form.tsx
@@ -5,14 +5,14 @@ const Form = () => {
     <div className={form.root}>
       <h2>Newsletter</h2>
       <div className={form.group}>
-        <input type="email" name="email" placeholder="Email" />
+        <input type="email" name="email" placeholder="Email" required />
         {/* <label>Email</label> */}
       </div>
       <div className={form.group}>
         <div className={form.inline}>
           <input type="text" name="name" placeholder="First Name" />
           {/* <label>First Name</label> */}
-          <input type="submit" className={form.btn} />
+        <input type="submit" className={form.btn} />
         </div>
       </div>
     </div>
