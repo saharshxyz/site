9a26dd09e4b7b12dab886f99bcb97e221d9ee91f
codefactor-io <support@codefactor.io>
2020-08-30 01:45:06 +0000

    [CodeFactor] Apply fixes to commit 4028d63

--- components/Blog/FeaturedPost.tsx
+++ components/Blog/FeaturedPost.tsx
@@ -3,7 +3,7 @@ import styles from './FeaturedPost.module.scss';
 const FeaturedPost = () => {
   return (
     <div className={styles.root}>
-			<h3>Lorem ipsum dolor sit amet.</h3>
+      <h3>Lorem ipsum dolor sit amet.</h3>
       <p>#tag 1 #tag2 #tag3</p>
     </div>
   );
