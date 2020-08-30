64ecf7f155a3dfb8e9dc7e2a217351156a685241
Saharsh Yeruva <hello@saharsh.tech>
2020-08-30 13:32:45 -0400

    Added page for posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import Layout from '../components/Layouts/Layout';
-import Post from '../components/Post';
+import Post from '../components/Copy';
 
 const Home = () => {
   return (
