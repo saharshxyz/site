a2b9601487744b955c72c401c658e6ee00e65f37
codefactor-io <support@codefactor.io>
2020-08-30 04:15:48 +0000

    [CodeFactor] Apply fixes to commit 4dd4d63

--- components/Blog/PostPreview.tsx
+++ components/Blog/PostPreview.tsx
@@ -5,8 +5,17 @@ const PostPreview = () => {
     <div className={postPreview.root}>
       <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
       <p className={postPreview.meta}>Aug 12 | 5 min | #stuff #things #life</p>
-			<hr/>
-			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam beatae nam repellendus ratione expedita possimus, sint dolore necessitatibus quisquam natus, nisi laborum magni sequi placeat ab perferendis, delectus reprehenderit similique ex veritatis illo! Quas voluptates ad exercitationem temporibus quibusdam, corrupti ratione dolorem optio alias. Animi adipisci quidem voluptates officiis provident deleniti officia! Dolor molestiae debitis maxime porro tempora eius architecto.</p>
+      <hr />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+        beatae nam repellendus ratione expedita possimus, sint dolore
+        necessitatibus quisquam natus, nisi laborum magni sequi placeat ab
+        perferendis, delectus reprehenderit similique ex veritatis illo! Quas
+        voluptates ad exercitationem temporibus quibusdam, corrupti ratione
+        dolorem optio alias. Animi adipisci quidem voluptates officiis provident
+        deleniti officia! Dolor molestiae debitis maxime porro tempora eius
+        architecto.
+      </p>
     </div>
   );
 };
