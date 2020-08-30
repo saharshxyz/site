4028d63f9c823c7743e05be9e5823b4860243fd0
Saharsh Yeruva <hello@saharsh.tech>
2020-08-29 21:44:28 -0400

    Added featured section on desktop - not responesive

--- components/Blog/Featured.tsx
+++ components/Blog/Featured.tsx
@@ -1,9 +1,27 @@
 import featured from './Featured.module.scss';
+import { FaBookmark } from 'react-icons/fa';
+import FeaturedPost from './FeaturedPost'
+import { features } from 'process';
 
 const Featured = () => {
   return (
-    <div>
-      <p>Hi</p>
+    <div className={featured.root}>
+      <div className={featured.wrapper}>
+        <h2>
+          <FaBookmark className={featured.icon} /> Featured
+        </h2>
+        <div className={featured.posts}>
+          <div className={featured.post}>
+            <FeaturedPost />
+          </div>
+          <div className={featured.post}>
+            <FeaturedPost />
+          </div>
+          <div className={featured.post}>
+            <FeaturedPost />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
