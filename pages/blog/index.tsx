9ff3b53da891a82cc8fe0a6ceee024b55be12434
Saharsh Yeruva <github@saharsh.tech>
2020-08-29 23:23:22 -0400

    fix tag filepath (#42)

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,6 +1,6 @@
 import Layout from '../../components/Layouts/Layout';
 import Header from '../../components/Blog/Header';
-import Tags from '../../components/Blog/tags';
+import Tags from '../../components/Blog/Tags';
 
 const Home = () => {
   return (
