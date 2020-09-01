02c592ffc7583d7c1f5bfc7ed7f9f3e37d300c95
Saharsh Yeruva <hello@saharsh.tech>
2020-09-01 18:04:11 -0400

    Attempting to generate paths to posts

---
+++ pages/blog/[slug].tsx
@@ -0,0 +1,49 @@
+import Layout from '../../components/Layouts/Layout';
+import Copy from '../../components/Copy';
+
+import { GetStaticProps, GetStaticPaths } from 'next';
+
+import { getPosts } from '../api/posts';
+
+const Post = (props) => {
+  return (
+    <Layout>
+      <h1>{props.post.title}</h1>
+      <p>{props.post.tagList}e</p>
+      <hr />
+      <Copy>{props.post.html}</Copy>
+    </Layout>
+  );
+};
+
+export default Post;
+
+export const getStaticProps = async () => {
+  const posts = await getPosts();
+
+  posts.map((post) => {
+    post.dateFormatted = new Intl.DateTimeFormat('en-US', {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+    }).format(new Date(post.published_at));
+  });
+
+  posts.map((post) => {
+    post.tagList = post.tags.map((tag) => tag.name).join(' ');
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
+
+export async function getStaticPaths() {
+  const posts = await getPosts();
+  return {
+    paths: posts?.map(({ post }) => `/blog/${post.slug}`),
+    fallback: false,
+  };
+}
