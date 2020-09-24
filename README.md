c5d5803f55ac3d8572c99d422891dfb9f6adf88d
Saharsh Yeruva <hello@saharsh.tech>
2020-09-23 22:14:36 -0400

    Add credits Should have done this a long time ago

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # [saharsh.tech](https://saharsh.tech)
 
-Source code for [saharsh.tech](https://saharsh.tech). Built using Nextjs.
+Source code for [saharsh.tech](https://saharsh.tech). Built using Nextjs. Design inspiration from [Linus](https://thesephist.com).
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,34 +3,37 @@ import { FaGithub, FaInstagram, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 import footer from './Footer.module.scss';
 
 const Footer = () => {
-  return (
-    <div className={footer.root}>
-      <h1>
-        <Link href="/">
-          <a className={footer.name}>Saharsh Yeruva</a>
-        </Link>
-      </h1>
-      <p>
-        <a href="https://srsh.link/twitter" className={footer.username}>
-          @saharshy29
-        </a>{' '}
-        |{' '}
-        <a href="https://srsh.link/github">
-          <FaGithub className={`${footer.icon} ${footer.github}`} />
-        </a>{' '}
-        <a href="https://srsh.link/instagram">
-          <FaInstagram className={`${footer.icon} ${footer.instagram}`} />
-        </a>{' '}
-        <a href="https://srsh.link/linkedin">
-          <FaLinkedin className={`${footer.icon} ${footer.linkedin}`} />
-        </a>{' '}
-        <a href="mailto:hello@saharsh.tech">
-          {' '}
-          <FaEnvelope className={`${footer.icon} ${footer.email}`} />{' '}
-        </a>
-      </p>
-    </div>
-  );
+	return (
+		<div className={footer.root}>
+			<h1>
+				<Link href='/'>
+					<a className={footer.name}>Saharsh Yeruva</a>
+				</Link>
+			</h1>
+			<p>
+				<a href='https://srsh.link/twitter' className={footer.username}>
+					@saharshy29
+				</a>{' '}
+				|{' '}
+				<a href='https://srsh.link/github'>
+					<FaGithub className={`${footer.icon} ${footer.github}`} />
+				</a>{' '}
+				<a href='https://srsh.link/instagram'>
+					<FaInstagram className={`${footer.icon} ${footer.instagram}`} />
+				</a>{' '}
+				<a href='https://srsh.link/linkedin'>
+					<FaLinkedin className={`${footer.icon} ${footer.linkedin}`} />
+				</a>{' '}
+				<a href='mailto:hello@saharsh.tech'>
+					{' '}
+					<FaEnvelope className={`${footer.icon} ${footer.email}`} />{' '}
+				</a>
+			</p>
+			<p className={footer.credits}>
+				Design inspiration from <a href='https://thesephist.com'>Linus</a>
+			</p>
+		</div>
+	);
 };
 
 export default Footer;
