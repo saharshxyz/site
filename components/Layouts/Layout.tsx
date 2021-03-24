37311f6e2b55120b996ef290703f864976ba71de
codefactor-io <support@codefactor.io>
2021-03-24 19:19:14 +0000

    [CodeFactor] Apply fixes to commit b9d55af

--- components/Layouts/Layout.tsx
+++ components/Layouts/Layout.tsx
@@ -3,20 +3,18 @@ import layout from './Layout.module.scss';
 import Nav from '../Nav';
 import Footer from '../Footer';
 
-const Layout = (props) => {
-  return (
-    <html lang="en">
-      <Head>
-        <title>Saharsh Yeruva | @saharshy29</title>
-      </Head>
-      <div className={layout.wrapper}>
-        <div className={layout.contentWrapper}>
-          <Nav />
-          {props.children}
-        </div>
+const Layout = (props) => (
+  <html lang="en">
+    <Head>
+      <title>Saharsh Yeruva | @saharshy29</title>
+    </Head>
+    <div className={layout.wrapper}>
+      <div className={layout.contentWrapper}>
+        <Nav />
+        {props.children}
       </div>
-    </html>
-  );
-};
+    </div>
+  </html>
+);
 
 export default Layout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,9 @@ const Home = () => (
   <Layout>
     <Post>
       <p>
-        Hey, this is Saharsh and you&#39;ll usually find me
-        going by <em>@saharshxyz</em> on the internet. Here are some of the places that you can find me:{' '}
+        Hey, this is Saharsh and you&#39;ll usually find me going by{' '}
+        <em>@saharshxyz</em> on the internet. Here are some of the places that
+        you can find me:{' '}
         <a href="https://sahar.sh/t" title="Saharsh&#39;s Twitter">
           Twitter
         </a>
@@ -19,7 +20,9 @@ const Home = () => (
         <a href="https://sahar.sh/ig" title="Saharsh&#39;s Intagram">
           Instagram
         </a>
-        , and <a href="https://sahar.sh/ln" title="Saharsh&#39;s Linkedin">Linkedin
+        , and{' '}
+        <a href="https://sahar.sh/ln" title="Saharsh&#39;s Linkedin">
+          Linkedin
         </a>
         . If you prefer, you can also reach out to me via email:{' '}
         <a href="mailto:hello@saharsh.tech" title="Saharsh&#39;s Email">
