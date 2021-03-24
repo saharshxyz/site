b9d55afae04316a3cc9cf0e4cb0227efcabba779
Saharsh Yeruva <hello@saharsh.tech>
2021-03-24 19:18:56 +0000

    Remove Footer

--- components/Layouts/Layout.tsx
+++ components/Layouts/Layout.tsx
@@ -14,7 +14,6 @@ const Layout = (props) => {
           <Nav />
           {props.children}
         </div>
-        <Footer />
       </div>
     </html>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -19,9 +19,7 @@ const Home = () => (
         <a href="https://sahar.sh/ig" title="Saharsh&#39;s Intagram">
           Instagram
         </a>
-        ,{' '}
-        <a href="https://sahar.sh/ln" title="Saharsh&#39;s Linkedin">
-          Linkedin
+        , and <a href="https://sahar.sh/ln" title="Saharsh&#39;s Linkedin">Linkedin
         </a>
         . If you prefer, you can also reach out to me via email:{' '}
         <a href="mailto:hello@saharsh.tech" title="Saharsh&#39;s Email">
