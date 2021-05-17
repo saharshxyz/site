74e93c2262f5745175f0595eeac7d2a0ed0eba5c
codefactor-io <support@codefactor.io>
2021-05-17 14:17:32 +0000

    [CodeFactor] Apply fixes to commit f0d1e3d

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,8 @@ const Home = () => (
   <Layout>
     <Post>
       <p>
-        Usually <em>@saharshxyz</em> on the internet. <a href="https://sahar.sh/t" title="Saharsh&#39;s Twitter">
+        Usually <em>@saharshxyz</em> on the internet.{' '}
+        <a href="https://sahar.sh/t" title="Saharsh&#39;s Twitter">
           Twitter
         </a>
         ,{' '}
@@ -17,7 +18,9 @@ const Home = () => (
         <a href="https://sahar.sh/ig" title="Saharsh&#39;s Intagram">
           Instagram
         </a>
-        , and <a href="https://sahar.sh/ln" title="Saharsh&#39;s Linkedin">Linkedin
+        , and{' '}
+        <a href="https://sahar.sh/ln" title="Saharsh&#39;s Linkedin">
+          Linkedin
         </a>
         . Email:{' '}
         <a href="mailto:hello@saharsh.xyz" title="Saharsh&#39;s Email">
