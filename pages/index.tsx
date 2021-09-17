771b0e148d4ee1c04d3dc11a124945cee6d8e6b0
codefactor-io <support@codefactor.io>
2021-09-17 01:25:30 +0000

    [CodeFactor] Apply fixes to commit 61d9d2a

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,14 @@ const Home = () => (
   <Layout>
     <Post>
       <p>
-          Oh you want to know more about me? Instead of just reading some random text blob about me here, shoot me an email saying 'hi' to <a href="mailto:hi@sahar.sh" title="Saharsh&#39;s Email">
-          hi [at] sahar.sh </a> and I'll send you my number so we can actually get to know each other. Don't be shy, I don't bite and I'll reply quickly, I promise :)
-      .</p>
-      
+        Oh you want to know more about me? Instead of just reading some random
+        text blob about me here, shoot me an email saying 'hi' to{' '}
+        <a href="mailto:hi@sahar.sh" title="Saharsh&#39;s Email">
+          hi [at] sahar.sh{' '}
+        </a>{' '}
+        and I'll send you my number so we can actually get to know each other.
+        Don't be shy, I don't bite and I'll reply quickly, I promise :) .
+      </p>
     </Post>
   </Layout>
 );
