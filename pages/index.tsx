61d9d2ad0413c0305a02f4fb2f151b733014f849
Saharsh Yeruva <github@saharsh.tech>
2021-09-16 21:25:17 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,26 +6,8 @@ const Home = () => (
   <Layout>
     <Post>
       <p>
-        Usually <em>@saharshxyz</em> on the internet:{' '}
-        <a href="https://sahar.sh/t" title="Saharsh&#39;s Twitter">
-          Twitter
-        </a>
-        ,{' '}
-        <a href="https://sahar.sh/gh" title="Saharsh&#39;s Github">
-          Github
-        </a>
-        ,{' '}
-        <a href="https://sahar.sh/ig" title="Saharsh&#39;s Intagram">
-          Instagram
-        </a>
-        ,{' '}
-        <a href="https://sahar.sh/ln" title="Saharsh&#39;s Linkedin">
-          Linkedin
-        </a>
-        .</p><p>Email:{' '}
-        <a href="mailto:hello@saharsh.xyz" title="Saharsh&#39;s Email">
-          hello [at] saharsh.xyz
-        </a>
+          Oh you want to know more about me? Instead of just reading some random text blob about me here, shoot me an email saying 'hi' to <a href="mailto:hi@sahar.sh" title="Saharsh&#39;s Email">
+          hi [at] sahar.sh </a> and I'll send you my number so we can actually get to know each other. Don't be shy, I don't bite and I'll reply quickly, I promise :)
       .</p>
       
     </Post>
