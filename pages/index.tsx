00e025921f2a090f189dcd3ed34e6dc341756344
Saharsh Yeruva <github@saharsh.tech>
2022-01-07 21:02:15 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,13 +6,9 @@ const Home = () => (
   <Layout>
     <Post>
       <p>
-        Instead of reading some random text blob about me, shoot me an email
-        saying "hi" to{' '}
         <a href="mailto:sup@sahar.sh" title="Saharsh&#39;s Email">
           sup [at] sahar.sh{' '}
-        </a>{' '}
-        and I'll send you my number so we can actually get to know each other.
-        Don't be shy, I don't bite and I'll reply quickly — I promise ;)
+        </a>
       </p>
     </Post>
   </Layout>
