43478201c579a19719d8a03e626e96f867da7c4b
Saharsh Yeruva <yeruva@saharsh.xyz>
2025-07-16 02:52:47 -0400

    deps: add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import prettier from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +13,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      prettier,
+    },
+    rules: {
+      "prettier/prettier": "warn",
+    },
+  },
+  eslintConfigPrettier,
 ];
 
 export default eslintConfig;
