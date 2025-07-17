bb8ecf9a59ec483d50898f70b82db03493654d0a
Saharsh Yeruva <yeruva@saharsh.xyz>
2025-07-17 17:38:23 -0400

    fix: badges overflow

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,13 @@ import {
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-center">
+    <main className="flex flex-col justify-center">
       <h1 className="text-2xl">Saharsh Yeruva</h1>
       <p className="mt-1">
         Computational Cognitive Science (Computer Science, Economics, Math and
         Statistics, Linguistics, Philosophy, and Psychology).
       </p>
-      <div className="mt-2.5 flex gap-2">
+      <div className="mt-2.5 flex flex-wrap gap-2">
         <IconBadge
           label="Twitter"
           link="https://sahar.sh/x"
